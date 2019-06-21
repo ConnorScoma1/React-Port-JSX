8816774a0e220646d88c258c88cfba73cbe71507
connor scoma <connorscoma@connors-MacBook-Pro.local>
2019-06-21 14:34:46 -0500

    adding

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -15,27 +15,41 @@ class About extends Component {
           </div>
           </Cell>
 
-          <Cell col={4}>
+          <Cell col={3}>
             <div className="leftSideAboutPage">
             <img 
               src = "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/13087288_10208265140899150_7003936357299384521_n.jpg?_nc_cat=104&_nc_ht=scontent-ort2-2.xx&oh=ca030da768048c8339f170ae9d1294c6&oe=5D51DB50"
               alt = "avatar-logo-img"
-              style = {{height: '250px'}}
+              style = {{height: '300px'}}
             />
           </div>
-          </Cell>
-          
+          </Cell> 
 
-          <Cell col={8}>
+          <Cell col={9}>
             <div className = "rightSideAboutPage" >
-            <h4>
-            Hello, my name is Connor Scoma. <br />
-            I am a Full Stack Web Developer and an entrepreneur. <br /> 
-            Feel free to take a look at my latest projects on my portfolio page.<br /> 
-            I have been developing as a freelancer for a few years. <br /> 
-            Studied at University of Wisconsin Waukesha and UWM Coding Boot Camp <br /> 
-            The best form of contacting me is on the contact page.<br /> 
+            <h3 id="aboutMeTextAnimation">
+            Hello, my name is Connor Scoma. <br /> 
+            I am a Software/Web Developer looking for challenging problems to solve. <br />
+            My background is in Fullstack Web development, <br />
+            however, I have an insatiable curiosity for deep learning – specifically <br />
+            applications and their potential to change the technology landscape. <br />
+            </h3>
+
+            <br />
+
+            <h4 id="aboutMeTextAnimation">
+           
+             I am passionate about building excellent applications that will improve day to day lifes.<br />
+             I specialize in creating web apps for clients, all <br />
+             of my work is produced locally. What would you do <br />
+             if you had a Web expert available at your fingertips?<br />
+            
             </h4>
+
+          <br />
+            <h3 id="aboutMeTextAnimation">
+            Don’t Worry When You Are Not Recognized, <br />but Strive to Be Worthy of Recognition
+            </h3>
             </div>
           </Cell>
 
--- src/components/resume.js
+++ src/components/resume.js
@@ -1,134 +1,140 @@
-import React, { Component } from 'react';
-import {Grid, Cell} from 'react-mdl';
-import Education from './education';
-import Experience from './experience'
-import Skills from './skills'
-
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
+import Education from "./education";
+import Experience from "./experience";
+import Skills from "./skills";
 
 class Resume extends Component {
   render() {
     return (
       <div id="list-page" className="transition-item">
-
         <Grid>
           <Cell col={4}>
-          <div className="LeftSideAnimation">
-            <div style={{textAlign: 'center'}}>
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
-                alt="avatar"
-                style={{height: '200px'}}
+            <div className="LeftSideAnimation">
+              <div style={{ textAlign: "center" }}>
+                <img
+                  src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"
+                  alt="avatar"
+                  style={{ height: "200px" }}
                 />
+              </div>
+
+              <h2 style={{ padding: "top" }}>Connor Scoma</h2>
+              <h4 style={{ color: "grey" }}>Let's Have Coffee & Code</h4>
+              <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+                <ul>
+                  <li>Proficient in HTML, JavaScript and CSS</li>
+                  <li>Experience with multiple databases</li>
+                  <li>Self-motivation with strong analytical abilities</li>
+                  <li>Excellent communication skills</li>
+                  <li>calability and performance optimization</li>
+                </ul>
+              <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
+              <h5>Address</h5>
+              <p>Brookfield, WI</p>
+              <h5>Phone</h5>
+              <p>(414) 837-0007</p>
+              <h5>E-Mail</h5>
+              <p>connorscoma@gmail.com</p>
+              <a
+                className="resumeAnchor"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://github.com/ConnorScoma1"
+              >
+                <h5>GitHub</h5>
+              </a>
+              <a
+                className="resumeAnchor"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://www.linkedin.com/in/connor-scoma-92730331/"
+              >
+                <h5>Linkedin</h5>
+              </a>
+
+              <a
+                className="resumeAnchor"
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://drive.google.com/file/d/1dtRqXBFU3uL0p7evDnIuxpHIAV8Nqz6l/view"
+              >
+                <h5>
+                  Resume
+                </h5>
+              </a>
+
+              <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
             </div>
-           
-              <h2 style={{padding: 'top'}}>Connor Scoma</h2>
-                <h4 style={{color: 'grey'}}>Programer</h4>
-                  <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
-                    <p>
-                        Hi, I’m Connor, I’ve been developing code for
-                        a very long time; studied Web Devolvement (Computer Science)
-                        at the University of Wisconsin Colleges. My portfolio site is
-                        filled with previously built websites along with my graphics
-                        created in Photoshop.
-                    </p>
-                  <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
-                    <h5>Address</h5>
-                      <p>Brookfield, WI</p>
-                    <h5>Phone</h5>
-                      <p>(414) 837-0007</p>
-                    <h5>E-Mail</h5>
-                      <p>connorscoma@gmail.com</p>
-                      <a className="resumeAnchor" rel = "noopener noreferrer" target="_blank"  href="https://github.com/ConnorScoma1">
-                    <h5>GitHub</h5>
-                      </a>
-                      <a className="resumeAnchor" rel = "noopener noreferrer" target="_blank"  href="https://www.linkedin.com/in/connor-scoma-92730331/">
-                    <h5>Linkedin</h5>
-                      </a>
-
-                      <a className="resumeAnchor" rel = "noopener noreferrer" target="_blank"  href="https://drive.google.com/file/d/1dtRqXBFU3uL0p7evDnIuxpHIAV8Nqz6l/view">
-                    <h5>Download <br /> Resume</h5>
-                      </a>
-                  
-
-                    <hr style={{borderTop: '3px solid #833fb2', width: '50%'}} />
-                  </div>
           </Cell>
-
-
 
           <Cell className="resume-right-col" col={8}>
             <h2>Education</h2>
 
-              <Education                   /*School # one */
-                startYear={2016}
-                endYear={2018}
-                schoolName="University of Wisconsin Colleges"
-                schoolDescription="At the University of Wisconsin Colleges, we were taught the most effective way to write our code to
-                make sure whoever is viewing can see it to its most potential. We learned the importance of validating
-                our code and why it’s important. From my time at the college I’ve also gains a lot of knowledge and
-                information about operating FTPs and Severs (we learned this with our PHP and MySQL courses). "
-              />
-                <hr style={{borderTop: '3px solid #fff' , width: '75%'}}/>
-
-                <h2>Experience</h2>
-                    <Experience
-                      startYear={2017}
-                      endYear="present"
-                      jobName="Freelance Web Development"
-                      jobDescription="I would be producing the website usually starting
-                      from the mobile site first and occasionally checking the site through the w3c validator to make sure
-                      the HTML has a strong foundation before adding styling and JavaScript."
-                    />
+            <Education /*School # one */
+              startYear="March 2019"
+              endYear="September 2019"
+              schoolName="UWM Coding Bootcamp"
+              schoolDescription="Create beautiful data-driven, interactive client-side web applications. 
+              JavaScript isn’t just for the front end. Create well-designed, 
+              comprehensively tested server-side JavaScript applications using Node and Express.
+              Built a foundation of computer science knowledge and honed my skills as an engineer,"
+            />
+          
 
 
-                    <Experience          /*JOB # Two */
-                      startYear={2016}
-                      endYear="present"
-                      jobName="Freelance Graphic Design"
-                      jobDescription="I mostly focused on Logo design and realist photomanipulation. With logo design I spent hours sketching
-out logos for clients that would be both unique and represent them. After the sketching process I would
-recreate the design in Illustrator so I could have a SVG of the logo. With the SVG I could create 4k and
-even 8k realist promotions for my clients. "
-                    />
+            <Education /*School # one */
+              startYear="September 2016"
+              endYear="June 2018"
+              schoolName="University of Wisconsin"
+              schoolDescription="At the University of Wisconsin Colleges, we were taught the most effective
+                 way to write our code to make sure whoever is viewing can see it to its most potential. 
+                 We learned the importance of validating our code and why it’s important. From my time 
+                 at the college, I’ve also gained a lot of knowledge and information about operating 
+                 FTPs and Severs (we learned this with our PHP and MySQL courses). "
+            />
+            <hr style={{ borderTop: "3px solid #fff", width: "75%" }} />
 
-                    <hr style={{borderTop: '3px solid #fff' , width: '50%'}}/>
+            <h2>Experience</h2>
+            <Experience
+              startYear={2017}
+              endYear="present"
+              jobName="Freelance Web Development"
+              jobDescription="Responsible for designing, coding 
+                      and modifying websites, from layout to function and 
+                      according to a client's specifications. Strive to create 
+                      visually appealing sites that feature user-friendly design 
+                      and clear navigation."
+            />
 
-                    <h2>Skills</h2>
+            <Experience /*JOB # Two */
+              startYear={2016}
+              endYear="present"
+              jobName="Freelance Graphic Design"
+              jobDescription="Create the visual versions of brands, 
+              messages and communications. They help people and businesses 
+              get their messages across in ways that are memorable, 
+              effective, and aesthetically pleasing."
+            />
 
-                      <Skills
-                        skill="HTML/CSS"
-                        progress={100}
-                      />
+            <hr style={{ borderTop: "3px solid #fff", width: "50%" }} />
 
-                      <Skills
-                        skill="React"
-                        progress={90}
-                      />
+            <h2>Top Skills</h2>
 
-                      <Skills
-                        skill="JavaScript"
-                        progress={80}
-                      />
+            <Skills skill="HTML/CSS" progress={100} />
 
-                      <Skills
-                        skill="C#"
-                        progress={70}
-                      />
+            <Skills skill="JavaScript" progress={100} />
 
-                      <Skills
-                        skill="NodeJS"
-                        progress={50}
-                      />
+            <Skills skill="NodeJS" progress={100} />
 
+            <Skills skill="SQL" progress={90} />
 
+            <Skills skill="Express" progress={80} />
 
           </Cell>
-
         </Grid>
-
-    </div>
-    )
-
+      </div>
+    );
   }
 }
 
